db1a9b292590031eb282b54d4dcf61d89f9c7c97
tkoukos <thanoskouk74@gmail.com>
2025-08-01 19:43:34 +0300

    solution for problem 1342 and pytest file

---
+++ problem1342/test_problem1342.py
@@ -0,0 +1,13 @@
+from problem1342 import Solution
+import pytest
+
+class TestSolution:
+
+    @pytest.mark.parametrize("num, expected", [
+        (14, 6),
+        (8, 4),
+        (123, 12)
+    ])
+
+    def test_numberOfSteps(self, num, expected):
+        assert Solution.numberOfSteps("", num) == expected
