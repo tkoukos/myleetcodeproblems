071ef4c0e90feeed183b085d9d0204ab0be6ad28
tkoukos <thanoskouk74@gmail.com>
2025-08-01 19:45:00 +0300

    solution for problem 2176 and pytest file

---
+++ problem2176/test_problem2176.py
@@ -0,0 +1,12 @@
+from problem2176 import Solution
+import pytest
+
+class TestSolution:
+
+    @pytest.mark.parametrize("nums, k, expected", [
+        ([3,1,2,2,2,1,3], 2, 4),
+        ([1,2,3,4], 1, 0),
+    ])
+
+    def test_countPairs(self, nums, k, expected):
+        assert Solution.countPairs("", nums, k) == expected
