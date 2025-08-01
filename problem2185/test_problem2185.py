78fcb7d156c96f977f1d0d6ab127d583dcc77f0c
tkoukos <thanoskouk74@gmail.com>
2025-08-01 19:45:20 +0300

    solution for problem 2185 and pytest file

---
+++ problem2185/test_problem2185.py
@@ -0,0 +1,12 @@
+from problem2185 import Solution
+import pytest
+
+class TestSolution:
+
+    @pytest.mark.parametrize("words, pref, expected", [
+        (["pay","attention","practice","attend"], "at", 2),
+        (["leetcode","win","loops","success"], "code", 0),
+    ])
+
+    def test_prefixCount(self, words, pref, expected):
+        assert Solution.prefixCount("", words, pref) == expected
