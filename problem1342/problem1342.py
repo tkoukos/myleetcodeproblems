bde7ef6ad267781b795fb96faf9deba131b31cd0
tkoukos <thanoskouk74@gmail.com>
2025-08-01 19:43:34 +0300

    solution for problem 1342 and pytest file

---
+++ problem1342/problem1342.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        step = 0
+        for i in range(1,num + 1):
+            if num%2==0:
+                num = num//2
+                if num == 0:
+                    break
+                step += 1
+            else:
+                num = num - 1
+                step += 1
+        return step
